40c7d9c44663a22570fb5cacbeead2b5773d7a0d
Alexi <an.prins@protonmail.com>
2024-01-09 10:31:05 +0100

    Begin working on event class

--- event.py
+++ event.py
@@ -1,10 +1,54 @@
-class Event:
+from datetime import datetime, timedelta
+import sqlite3
 
-    def __init__(self) -> None:
+from skater import Skater
+from track import Track
+
+
+class Event:
+    def __init__(self, id: int, name: str, track_id: int, date: datetime, distance: int, duration: float, laps: int,
+                 winner: str, category: str) -> None:
+        self.id = id
+        self.name = name
+        self.track_id = track_id
+        self.date = date
+        self.distance = distance
+        self.duration = duration
+        self.laps = laps
+        self.winner = winner
+        self.category = category
+
+    def add_skater(self, skater: Skater):
+        conn = sqlite3.connect('iceskatingsapp.db')
+        query = """
+INSERT INTO event_skaters (skater_id, event_id)
+SELECT ?, ?
+WHERE NOT EXISTS (SELECT 1 FROM event_skaters WHERE skater_id = ? AND event_id = ?)
+                """
+        conn.execute(query, [skater.id, self.id, skater.id, self.id])
+        conn.commit()
+
+    def get_skaters(self) -> list:
+        conn = sqlite3.connect('iceskatingapp.db')
+        cursor = conn.cursor()
+        cursor.execute(f"""
+SELECT * FROM skaters WHERE id IN (SELECT skater_id FROM event_skaters WHERE event_id = {self.id})
+                        """)
+        fetch_skaters = cursor.fetchall()
+        return fetch_skaters
+
+    def get_track(self) -> Track:
+        pass
+
+    def convert_date(self, to_format: str) -> str:
+        return datetime.strftime(self.date, to_format)
+
+    def convert_duration(self, to_format: str) -> str:
         pass
 
     # Representation method
     # This will format the output in the correct order
     # Format is @dataclass-style: Classname(attr=value, attr2=value2, ...)
     def __repr__(self) -> str:
-        return "{}({})".format(type(self).__name__, ", ".join([f"{key}={value!s}" for key, value in self.__dict__.items()]))
+        return "{}({})".format(type(self).__name__, ", ".join([f"{key}={value!s}"
+                                                               for key, value in self.__dict__.items()]))
